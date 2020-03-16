ceef9a87968245e42af8dd5cb4390b4f990ba20a
Anastaiya Remontova <a.remontova@makeapp.ru>
2020-03-16 14:09:54 +0300

    Added cookie fragment and layout

---
+++ app/src/main/java/com/clickerhunt/cookieclicker/cookie/CookieFragment.kt
@@ -0,0 +1,7 @@
+package com.clickerhunt.cookieclicker.cookie
+
+import androidx.fragment.app.Fragment
+import com.clickerhunt.cookieclicker.R
+
+class CookieFragment : Fragment(R.layout.fragment_cookie) {
+}
