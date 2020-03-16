11c48f6b04ea466e329f69ad1b58bca34ac081b4
Anastaiya Remontova <a.remontova@makeapp.ru>
2020-03-16 14:10:08 +0300

    Updated navigation

--- app/src/main/java/com/clickerhunt/cookieclicker/home/HomeFragment.kt
+++ app/src/main/java/com/clickerhunt/cookieclicker/home/HomeFragment.kt
@@ -1,9 +1,31 @@
 package com.clickerhunt.cookieclicker.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.clickerhunt.cookieclicker.R
+import com.clickerhunt.cookieclicker.cookie.CookieFragment
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        displayCookie()
+    }
+
+    private fun displayCookie() {
+        val fragmentTransaction = childFragmentManager.beginTransaction()
+        fragmentTransaction.replace(
+            R.id.placeholder,
+            CookieFragment()
+        )
+        fragmentTransaction.commit()
+    }
 
 
 }
