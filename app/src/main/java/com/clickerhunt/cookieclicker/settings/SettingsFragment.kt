005ca1ac4c314b77a5cf2d0608cbd0ba9abdf031
Anastaiya Remontova <a.remontova@makeapp.ru>
2020-03-16 17:12:59 +0300

    Added settings fragment and layout

---
+++ app/src/main/java/com/clickerhunt/cookieclicker/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.clickerhunt.cookieclicker.settings
+
+import androidx.fragment.app.Fragment
+import com.clickerhunt.cookieclicker.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+}
