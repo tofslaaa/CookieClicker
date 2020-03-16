95857acaf3029d3edabef4a2b745f4e66dc1f8da
Anastaiya Remontova <a.remontova@makeapp.ru>
2020-03-16 16:19:14 +0300

    Added shop fragment and layout

---
+++ app/src/main/java/com/clickerhunt/cookieclicker/shop/ShopFragment.kt
@@ -0,0 +1,7 @@
+package com.clickerhunt.cookieclicker.shop
+
+import androidx.fragment.app.Fragment
+import com.clickerhunt.cookieclicker.R
+
+class ShopFragment : Fragment(R.layout.fragment_shop) {
+}
