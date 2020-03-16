5cd146f7be83001a65c63b332e125cb4b39544ef
Anastaiya Remontova <a.remontova@makeapp.ru>
2020-03-16 16:42:27 +0300

    Updated navigation

--- app/src/main/java/com/clickerhunt/cookieclicker/home/HomeFragment.kt
+++ app/src/main/java/com/clickerhunt/cookieclicker/home/HomeFragment.kt
@@ -3,14 +3,18 @@ package com.clickerhunt.cookieclicker.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.clickerhunt.cookieclicker.R
 import com.clickerhunt.cookieclicker.cookie.CookieFragment
+import com.clickerhunt.cookieclicker.shop.ShopFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        shop_open_button.setOnClickListener { displayShop() }
+        shop_close_button.setOnClickListener { displayCookie() }
     }
 
     override fun onStart() {
@@ -25,6 +29,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             CookieFragment()
         )
         fragmentTransaction.commit()
+        shop_open_button.visibility = View.VISIBLE
+        shop_close_button.visibility = View.INVISIBLE
+    }
+
+    private fun displayShop() {
+        val fragmentTransaction = childFragmentManager.beginTransaction()
+        fragmentTransaction.replace(
+            R.id.placeholder,
+            ShopFragment()
+        )
+        fragmentTransaction.commit()
+        shop_open_button.visibility = View.INVISIBLE
+        shop_close_button.visibility = View.VISIBLE
     }
 
 
