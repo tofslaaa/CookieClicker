a1c62f951e102ae9234d7a6c9ca79db8eddaff16
Anastaiya Remontova <a.remontova@makeapp.ru>
2020-03-17 20:44:19 +0300

    Implemented settings saving and getting logic

--- app/src/main/java/com/clickerhunt/cookieclicker/settings/SettingsFragment.kt
+++ app/src/main/java/com/clickerhunt/cookieclicker/settings/SettingsFragment.kt
@@ -5,13 +5,37 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.clickerhunt.cookieclicker.R
+import com.clickerhunt.cookieclicker.database.AppDatabase
+import com.clickerhunt.cookieclicker.database.Configuration
 import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private val settings by lazy { AppDatabase.invoke(requireContext()).configurationDao() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         settings_close_button.setOnClickListener { it.findNavController().popBackStack() }
+
+        sound_switch.setOnCheckedChangeListener { _, isChecked ->
+            settings.upsert(Configuration(soundIsOn = isChecked))
+        }
+        vibration_switch.setOnCheckedChangeListener { _, isChecked ->
+            settings.upsert(Configuration(vibrationIsOn = isChecked))
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        updateViews()
+    }
+
+    private fun updateViews() {
+        if (settings.getSettings() != null) {
+            sound_switch.isChecked = settings.getSettings().soundIsOn
+            vibration_switch.isChecked = settings.getSettings().vibrationIsOn
+        }
+    }
+
 }
